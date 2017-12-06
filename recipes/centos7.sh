c1851372d3fa8ddaa5fca47495cf7ff473dfa04f
rlichtenwalter <rlichtenwalter@gmail.com>
2017-12-06 12:21:20 -0500

    Create centos7.sh

---
+++ recipes/centos7.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# ASSUMES root USER ON RECENT CENTOS 7 MINIMAL INSTALL
+yum -y update
+yum -y install wget unzip gcc perl dos2unix
+yum -y install epel-release
+yum -y install https://download.postgresql.org/pub/repos/yum/9.4/redhat/rhel-7-x86_64/pgdg-centos94-9.4-3.noarch.rpm
+yum -y install postgresql94 postgresql94-server postgresql94-contrib postgresql94-libs postgresql94-devel
+export PATH=/usr/pgsql-9.4/bin:$PATH
+postgresql94-setup initdb
+systemctl enable postgresql-9.4.service
+systemctl start postgresql-9.4.service
+chkconfig postgresql-9.4 on
+wget https://github.com/rlichtenwalter/pgsql_genomics/archive/master.zip
+unzip master.zip
+cd pgsql_genomics-master
+./setup.sh
