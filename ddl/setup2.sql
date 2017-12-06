521e26948fa988d8b1587c0ef37474309adbc2bb
rlichtenwalter <rlichtenwalter@gmail.com>
2017-12-06 11:46:18 -0500

    Fixing error in path to .so
    Path suffix to $libdir was changed to the project-correct 'pgsql_genomics' from 'user_defined'

--- ddl/setup2.sql
+++ ddl/setup2.sql
@@ -2,5 +2,5 @@
 
 CREATE OR REPLACE FUNCTION summarize_variant( TINYINT[], INTEGER[] DEFAULT NULL )
 	RETURNS variant_data
-	AS '$libdir/user_defined/summarize_variant'
+	AS '$libdir/pgsql_genomics/summarize_variant'
 	LANGUAGE C IMMUTABLE;
